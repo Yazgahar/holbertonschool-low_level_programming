583c540445b3d3a832f838a79db9add9986d8c38
yazgahar <battianas@hotmail.com>
2023-10-17 16:30:24 +0200

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alph in low ten times
+ *
+ * Return: 1 or 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
