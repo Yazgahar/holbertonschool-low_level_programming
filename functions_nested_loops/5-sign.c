043057508ead971e4334f123d9a664f8a73b8a0e
yazgahar <battianas@hotmail.com>
2023-10-17 17:27:49 +0200

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,12 +7,15 @@
  */
 int print_sign(int n)
 {
-	if (n > O)
+	int n1;
+
+	n1 = 0;
+	if (n > n1)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n == n1)
 	{
 		_putchar('0');
 		return (0);
@@ -22,5 +25,4 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	return (0);
 }
