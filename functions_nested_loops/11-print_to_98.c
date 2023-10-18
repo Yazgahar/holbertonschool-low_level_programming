1ecd86e5b01428df9d6557779782885a99dbec19
yazgahar <battianas@hotmail.com>
2023-10-18 20:02:19 +0200

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,14 @@ void print_to_98(int n)
 {
 	if (n == 98)
 		printf("%d", n);
-	while (n > 98)
+	while (n >= 98)
 	{
 		printf("%d", n);
 		if (n != 98)
 			printf(", ");
 		n--;
 	}
-	while (n < 98)
+	while (n <= 98)
 	{
 		printf("%d", n);
 		if (n != 98)
