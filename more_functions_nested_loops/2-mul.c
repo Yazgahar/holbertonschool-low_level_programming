08bac5a912a812ccf5e278d717b8667445d49d64
yazgahar <battianas@hotmail.com>
2023-10-20 10:17:19 +0200

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - mul 2 integers
+ * @a: compare parameter
+ * @b: compare parameter
+ *
+ * Return: result of mul a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
