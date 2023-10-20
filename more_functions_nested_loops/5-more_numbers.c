7d8fb60c242d2becdfc686deb398e7426bd32481
yazgahar <battianas@hotmail.com>
2023-10-20 12:01:01 +0200

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,18 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int j = 0;
+	int i, j = 0;
 
 	while (j < 10)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 		}
 		j++;
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
