5b4e5c1ee8e74f161015f36d604613106aee089d
yazgahar <battianas@hotmail.com>
2023-10-22 17:38:17 +0200

    102-fibonacci.c

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int a = 1, b = 2, next;
+	int count = 2;
+
+	printf("%ld, %ld ", a, b);
+
+	while (count < 50)
+	{
+		next = a + b;
+		printf("%ld, ", next);
+
+		a = b;
+		b = next;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
+
