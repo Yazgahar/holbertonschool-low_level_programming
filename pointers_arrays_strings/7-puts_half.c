58e800d7715670782c878c39f11b1b2c0757d07e
yazgahar <battianas@hotmail.com>
2023-10-23 15:26:52 +0200

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,15 @@ void puts_half(char *str)
 	len = 0;
 	while (str[len] != '\0')
 		len++;
-	for (i = len / 2; i < len; i++)
-		_putchar(str[i]);
+	if (len % 2 == 1)
+	{	
+		for (i = (len - 1) / 2; i < len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = len / 2; i < len; i++)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
