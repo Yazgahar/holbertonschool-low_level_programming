e0f3993b4be8aafb9de7d7112b31fdd67d17de93
yazgahar <battianas@hotmail.com>
2023-10-23 17:49:11 +0200

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers.
+ * @a: compare parameter.
+ * @n: compare parameter.
+ *
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
