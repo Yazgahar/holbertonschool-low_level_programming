6701c3f1e3ebb6f22b4c9dc8066adaf0760af3bb
yazgahar <battianas@hotmail.com>
2023-10-23 11:29:36 +0200

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse.
+ * @s: compare parameter.
+ *
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
