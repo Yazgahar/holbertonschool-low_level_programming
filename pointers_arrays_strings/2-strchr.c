d579f82863f2030ffcbbefaa24c4636f4447280b
yazgahar <battianas@hotmail.com>
2023-10-28 18:00:09 +0200

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string.
+ * @s: compare parameter.
+ * @c: compare parameter.
+ *
+ * Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ocr;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			ocr = &(s[i]);
+			return (ocr);
+		}
+	}
+	return (0);
+}
