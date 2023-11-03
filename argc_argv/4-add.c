9ae77aa87969b2ab8378241d92d6b3a4613bdf81
yazgahar <battianas@hotmail.com>
2023-11-03 20:39:39 +0100

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * non_digit_check - check for a non digit char in the string
+ * @str: the string
+ *
+ * Return: 1 if the char is a non digit, else 0.
+ */
+int non_digit_check(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (1);
+	}
+	return (0);
+}
+/**
  * main - check the code
  * @argc: count of the arguments
  * @argv: an array of strings
@@ -22,6 +39,12 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	result = 0;
+	for (i = 1; i < argc; i++)
+		if (non_digit_check(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
 	for (i = 1; i < argc; i++)
 	{
 		nb =  atoi(argv[i]);
