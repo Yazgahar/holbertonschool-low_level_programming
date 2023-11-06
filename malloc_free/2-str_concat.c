5343d6e32df8e5354c5273b50b4db0b1a49bc321
yazgahar <battianas@hotmail.com>
2023-11-06 19:09:37 +0100

    2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,68 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: compare parameter.
+ * @src: compare parameter.
+ *
+ * Return: pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *originalDest = dest;
+
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (originalDest);
+}
+/**
+ * _strcpy - copies a string.
+ * @dest: compare parameter.
+ * @src: compare parameter.
+ *
+ * Return: void.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * str_concat - concatenates two strings.
+ * @s1: compare parameter.
+ * @s2: compare parameter.
+ *
+ * Return: char.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *res;
+
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	j = 0;
+	while (s2[j] != '\0')
+		j++;
+	res = malloc((i + j + 1) * sizeof(char));
+	if ((i + j) == 0)
+		return (0);
+	_strcpy(res, s1);
+	_strcat(res, s2);
+	return (res);
+}
