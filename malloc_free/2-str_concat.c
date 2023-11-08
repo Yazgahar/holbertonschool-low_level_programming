f47ce5e7932ed817871a03f0a2448e9af7b0ff26
yazgahar <battianas@hotmail.com>
2023-11-08 22:40:54 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -53,9 +53,9 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *res;
 
-	if (s1 == NULL)
+	if (s1 == 0)
 		s1 = "";
-	if (s2 == NULL)
+	if (s2 == 0)
 		s2 = "";
 	i = 0;
 	while (s1[i] != '\0')
