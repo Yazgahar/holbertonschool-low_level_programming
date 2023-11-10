2828f18a5c2f007a918a42dc669ead2127ebd352
yazgahar <battianas@hotmail.com>
2023-11-10 11:59:36 +0100

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,17 +3,17 @@
  * _calloc - allocates memory for an array
  * @nmemb: compare parameter
  * @size: compare parameter
- * Return: a pointer
+ * Return: a void pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	void *ptr;
 
 	if (nmemb == 0)
 		return (0);
 	if (size == 0)
 		return (0);
-	ptr = malloc(nmemb * size);
+	ptr = (void *) malloc(nmemb * size);
 	if (ptr != 0)
 		memset(ptr, 0, nmemb * size);
 	if (ptr == 0)
