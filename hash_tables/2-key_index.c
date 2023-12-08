8fd484446d099460548a3ad6a0e0dc6edcd04e3f
yazgahar <battianas@hotmail.com>
2023-12-08 17:52:25 +0100

    2-key_index.c

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * key_index - return the index of a key
+ * @key: key to check the index of
+ * @size: size of the array of the hash table
+ * Return: the index of the key
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	if (!key || size == 0)
+		return (0);
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
