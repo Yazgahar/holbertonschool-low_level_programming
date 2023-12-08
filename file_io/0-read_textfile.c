c045bbd0b15dd2cd5bc323cdd661b397fc7d9e03
yazgahar <battianas@hotmail.com>
2023-12-08 17:28:42 +0100

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,31 +1,44 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: size of text
- * Return: the actual number of letters it could read and print
+ * read_textfile - read a text file and prints it to the posix std output
+ * @filename: file to read
+ * @letters: number of letters it should read and print
+ * Return: 0 if filename is NULL,if file can't be opened and read,if write fail
+ * or does not write the expected amount of bytes else return number of
+ * letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, read, write;
+	int fd;
+	ssize_t nbr_read, nbr_written;
 	char *buffer;
 
-	buffer = malloc(letters * sizeof(char));
-
-	if (!buffer)
+	if (!filename)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
-	read = read(fd, buffer, letters);
-	write = write(STDOUT_FILENO, buffer, read);
-
-	if (fd == -1 || read == -1 || write != read)
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters * sizeof(char));
+	if (!buffer)
 	{
-		free(buffer);
+		close(fd);
 		return (0);
 	}
+	nbr_read = read(fd, buffer, letters);
+	if (nbr_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	nbr_written = write(STDOUT_FILENO, buffer, nbr_read);
+
+	if (nbr_written == -1)
+		return (0);
+
+	free(buffer);
 	close(fd);
-	return (write);
+	return (nbr_written);
 }
